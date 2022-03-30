308948d3840b565b9615b60004637dd75b8a3341
proudkittapa <proudy2013@gmail.com>
2022-03-30 12:29:05 +0700

    -

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 import paho.mqtt.client as mqtt
 import random
 import time
-from apscheduler.schedulers.background import BackgroundScheduler
-from flask_socketio import SocketIO, emit
 
 broker = 'broker.emqx.io'
 port = 1883
@@ -13,45 +11,12 @@ client_id = f'python-mqtt-{random.randint(0, 1000)}'
 username = 'emqx'
 password = 'public'
 
-testData = ""
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    client.subscribe(topic)
-
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload.decode()) + '\n')
-    global testData 
-    testData = str(msg.payload.decode())
-    socketio.emit('update',testData, broadcast=True)
-
-#schedule job
-
-def get_data():
-    on_message
-    return testData
-
-scheduler = BackgroundScheduler()
-running_job = scheduler.add_job(get_data, 'interval', seconds=1, max_instances=1)
-scheduler.start()
-
-def on_disconnect(client, userdata, rc):
-    print("Closing data file...")
-
-client = mqtt.Client(client_id=client_id)
-client.on_connect = on_connect
-client.on_message = on_message
-client.on_disconnect = on_disconnect
-client.username_pw_set(username, password)
-
-client.connect(broker, port, 60)
-client.loop_start()
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@127.0.0.1:8889/window62"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-socketio = SocketIO(app)
 db = SQLAlchemy(app)
+
+testData = ""
 
 class Object(db.Model):
     obj_id = db.Column(db.Integer, primary_key=True)
@@ -72,6 +37,56 @@ class Sensor(db.Model):
     def __repr__(self):
         return '<Sensor %r>' % self.sensor_name
 
+class temp_reading(db.Model):
+    temp_id = db.Column(db.Integer, primary_key=True)
+    temp_input = db.Column(db.Float)
+    sensor_id = db.Column(db.Integer)
+    def __repr__(self):
+        return '<Sensor %r>' % self.temp_id
+
+class ldr_reading(db.Model):
+    ldr_id = db.Column(db.Integer, primary_key=True)
+    ldr_input = db.Column(db.Float)
+    sensor_id = db.Column(db.Integer)
+    def __repr__(self):
+        return '<Sensor %r>' % self.ldr_id
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe(topic)
+    client.subscribe("WindowLight")
+    client.subscribe("WindowTemp")
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload.decode()) + '\n')
+    if msg.topic == "WindowTemp":
+        # temp = temp_reading(sensor_id=3, temp_input=3.3)
+        # insert(temp)
+        print("Temp", msg.payload.decode())
+    elif msg.topic == "WindowLight":
+        # light = ldr_reading(sensor_id=1, ldr_input=543)
+        # insert(light)
+        print("Light", msg.payload.decode())
+    global testData 
+    testData = str(msg.payload.decode())
+
+def on_disconnect(client, userdata, rc):
+    print("Closing data file...")
+
+client = mqtt.Client(client_id=client_id)
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_disconnect = on_disconnect
+client.username_pw_set(username, password)
+
+client.connect(broker, port, 60)
+client.loop_start()
+
+def insert(value):
+    db.session.add(value)
+    db.session.commit()
+
+
 @app.route('/')
 def index():
     if request.method == "GET":
@@ -85,7 +100,6 @@ def sensors(id):
         sensors = Sensor.query.filter_by(obj_id=id).all()
         return render_template("sensor.html", sensors=sensors, testData=testData)
 
-
 if __name__ == '__main__':
     client = mqtt.Client()
-    socketio.run(app, debug=True, host='localhost', port=5555)
+    app.run( debug=True, host='localhost', port=5555)
